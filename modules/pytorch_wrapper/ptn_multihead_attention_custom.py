2168836d13b7a58cd3d7ebf0b33c99c7ceb5f949
HowToSD <support@candee.ai>
2025-04-07 19:37:13 -0700

    Fixed the docstring for MHA custom class

--- modules/pytorch_wrapper/ptn_multihead_attention_custom.py
+++ modules/pytorch_wrapper/ptn_multihead_attention_custom.py
@@ -6,7 +6,7 @@ from einops import einops
 
 class PtnMultiheadAttentionCustomDef(nn.Module):
     """
-    Implements MultiheadAttention.
+    Implements Multihead attention.
 
     Attributes:
         embed_dim (int): Hidden dimension (d_model) for each token of the Transformer.
